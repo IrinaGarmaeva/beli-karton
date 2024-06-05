65a1c09dc625cad3e8435b8fd4d725d4101429a6
Irina Garmaeva <garmaeva-i@mail.ru>
2024-06-05 18:17:19 +0200

    Finish Form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,57 @@
+import { useState, useEffect } from "react";
+import PrinterIcon from "../assets/icons/icon-printer.png";
+
 const Form = () => {
+  const [registrationDate, setRegistrationDate] = useState<string>("");
+
+  useEffect(() => {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    const mm = String(today.getMonth() + 1).padStart(2, "0");
+    const dd = String(today.getDate()).padStart(2, "0");
+    const formattedToday = `${yyyy}-${mm}-${dd}`;
+    setRegistrationDate(formattedToday);
+  }, []);
+
   const handlePrint = () => {
     window.print();
   };
 
   return (
     <div className="flex flex-col justify-center text-xxs w-full">
-      <p className="text-right">Образац 1.</p>
+      <div className="flex flex-col justify-center items-center print:hidden">
+        <h1 className="text-sm text-center">
+          Автоматическое создание белого картона для прибывших в Сербию после
+          визарана.
+        </h1>
+        <p className="text-xs text-center">
+          Форма не собирает персональные данные и не хранит их. Cookies не
+          используются
+        </p>
+        <button
+          type="button"
+          onClick={handlePrint}
+          className="mt-2 flex flex-col justify-center items-center"
+        >
+          <img
+            src={PrinterIcon}
+            alt="Иконка печати документа"
+            className="w-5"
+          />
+          <p className="mt-2 text-xs">Напечатать форму</p>
+        </button>
+      </div>
+      <p className="text-right text-xs pb-1">Образац 1.</p>
       <div className="flex flex-col justify-center p-2 border border-1 border-black border-dotted w-full">
         <div className="flex justify-center relative">
-          <p className="uppercase text-center">ПРИЈАВА БОРАВИШТА СТРАНЦА</p>
-          <p className="absolute right-7">Серијски број/Serial number:</p>
+          <p className="uppercase text-center pb-1">
+            ПРИЈАВА БОРАВИШТА СТРАНЦА
+          </p>
+          <p className="absolute right-9">Серијски број/Serial number:</p>
         </div>
-        <p className="uppercase text-center">REGISTRATION OF PLACE OF STAY</p>
+        <p className="uppercase text-center pb-1">
+          REGISTRATION OF PLACE OF STAY
+        </p>
         <form className="rounded shadow-md">
           <table className="min-w-full border border-1 border-black">
             <tbody>
@@ -25,6 +65,7 @@ const Form = () => {
                     name="surname"
                     className="p-1 w-full text-sm"
                     placeholder="Введите ФАМИЛИЮ как в загранпаспорте"
+                    required
                   />
                 </td>
               </tr>
@@ -38,6 +79,7 @@ const Form = () => {
                     name="name"
                     className="p-1 w-full text-sm"
                     placeholder="Введите ИМЯ как в загранпаспорте"
+                    required
                   />
                 </td>
               </tr>
@@ -57,7 +99,8 @@ const Form = () => {
                   <input
                     type="date"
                     name="dateOfBirth"
-                    className="p-1 w-full text-sm"
+                    className="p-1 text-sm"
+                    required
                   />
                 </td>
               </tr>
@@ -69,7 +112,17 @@ const Form = () => {
                   </div>
                 </td>
                 <td className="p-2 border border-black">
-                  <input type="text" name="sex" className="p-1 w-full text-sm" />
+                  <select
+                    name="sex"
+                    className="p-1 text-sm print:border-none"
+                    required
+                  >
+                    <option value="" disabled selected>
+                      Выберите пол
+                    </option>
+                    <option value="Muški">Muški</option>
+                    <option value="Ženski">Ženski</option>
+                  </select>
                 </td>
               </tr>
               <tr className="border border-black">
@@ -84,7 +137,9 @@ const Form = () => {
                     type="text"
                     name="birthPlace"
                     className="p-1 w-full text-sm"
-                    defaultValue="Rusija"
+                    defaultValue="Russia"
+                    placeholder="Введите страну рождения как в загранпаспорте"
+                    required
                   />
                 </td>
               </tr>
@@ -100,7 +155,8 @@ const Form = () => {
                     type="text"
                     name="nationality"
                     className="p-1 w-full text-sm"
-                    defaultValue="Rusko"
+                    defaultValue="Russian"
+                    placeholder="Введите гражданство"
                   />
                 </td>
               </tr>
@@ -115,7 +171,7 @@ const Form = () => {
                   <input
                     type="text"
                     name="travelDocument"
-                    className="p-2 w-full"
+                    className="p-1 w-full text-sm"
                     placeholder="Введите номер загранпаспорта"
                   />
                 </td>
@@ -127,8 +183,11 @@ const Form = () => {
                     <span>Type and number of visa and place of issuance</span>
                   </div>
                 </td>
-                <td className="p-5 border border-black">
-                  <input type="text" name="visa" className="p-4 w-full" />
+                <td className="p-2 border border-black">
+                  <textarea
+                    name="visa"
+                    className="px-1 w-full text-sm h-12 overflow-hidden resize-none"
+                  ></textarea>
                 </td>
               </tr>
               <tr className="border border-black">
@@ -140,21 +199,49 @@ const Form = () => {
                 </td>
                 <td className="p-2 border border-black">
                   <input
-                    type="text"
-                    name="entryDatePlace"
-                    className="p-2 w-full"
+                    type="date"
+                    className="p-1 text-sm"
+                    value={registrationDate}
+                    onChange={(e) => setRegistrationDate(e.target.value)}
+                    required
                   />
+                  <select
+                    name="borderPoint"
+                    className="p-1 w-4/5 text-sm print:border-none"
+                    required
+                  >
+                    <option value="" disabled selected>
+                      Выберите пограничный пункт
+                    </option>
+                    <option value="Sremska Rača">Sremska Rača</option>
+                    <option value="Beograd">Beograd</option>
+                    <option value="Bačka Palanka">Bačka Palanka</option>
+                    <option value="Bajina Basta">Bajina Basta</option>
+                    <option value="Brodarevo">Brodarevo</option>
+                    <option value="Gostun">Gostun</option>
+                    <option value="Kotroman">Kotroman</option>
+                    <option value="Ljubovija">Ljubovija</option>
+                    <option value="Јамеna">Јамеna</option>
+                    <option value="Uvac">Uvac</option>
+                    <option value="Subotica">Subotica</option>
+                  </select>
                 </td>
               </tr>
               <tr className="border border-black">
                 <td className="p-2 border border-black">
                   <div className="flex flex-col">
                     <span>Адреса боравишта у Републици Србији</span>
-                    <span>Аddress of place of stay in the Republic of Serbia</span>
+                    <span>
+                      Аddress of place of stay in the Republic of Serbia
+                    </span>
                   </div>
                 </td>
-                <td className="p-7 border border-black">
-                  <input type="text" name="address" className="p-2 w-full" />
+                <td className="p-2 border border-black">
+                  <textarea
+                    name="address"
+                    className="px-1 w-full text-sm h-14 overflow-hidden resize-none"
+                    placeholder="Введите адрес регистрации в Сербии"
+                  ></textarea>
                 </td>
               </tr>
               <tr className="border border-black">
@@ -174,8 +261,17 @@ const Form = () => {
                 <td className="p-2 border border-black">
                   <input
                     type="text"
+                    name="landlordName"
+                    className="p-1 w-full text-sm"
+                    placeholder="Введите фамилию и имя арендодателя"
+                    required
+                  />
+                  <input
+                    type="text"
                     name="landlordInfo"
-                    className="p-2 w-full"
+                    className="p-1 w-full text-sm"
+                    placeholder="Введите номер ID-карты арендодателя,пример JMBG0123456789012"
+                    required
                   />
                 </td>
               </tr>
@@ -190,7 +286,10 @@ const Form = () => {
                   <input
                     type="date"
                     name="registrationDate"
-                    className="p-2 w-full"
+                    className="p-1 text-sm"
+                    value={registrationDate}
+                    onChange={(e) => setRegistrationDate(e.target.value)}
+                    required
                   />
                 </td>
               </tr>
@@ -201,8 +300,8 @@ const Form = () => {
                     <span>Note</span>
                   </div>
                 </td>
-                <td className="p-8 border border-black">
-                  <textarea name="note" className="p-2 w-full"></textarea>
+                <td className="p-2 border border-black">
+                  <textarea name="note" className="p-1 w-full h-20"></textarea>
                 </td>
               </tr>
               <tr className="border border-black">
@@ -220,7 +319,9 @@ const Form = () => {
                 <td className="p-2 border border-black"></td>
                 <td className="p-2 border border-black">
                   <div className="flex flex-col items-center">
-                    <p className="pt-7 w-full text-center">________________________________________</p>
+                    <p className="pt-8 w-full text-center">
+                      ________________________________________
+                    </p>
                     <span>
                       (потпис овлашћеног лица - signature of the authorized
                       person)
@@ -230,14 +331,6 @@ const Form = () => {
               </tr>
             </tbody>
           </table>
-
-          <button
-            type="button"
-            onClick={handlePrint}
-            className="mt-4 bg-blue-500 text-white py-2 px-4 rounded"
-          >
-            Print Form
-          </button>
         </form>
       </div>
     </div>
